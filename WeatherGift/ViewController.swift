2a37ece8e60736a514859d8d3af7a2da99fa3bfc
John Pappas <johncpappas4@gmail.com>
2020-10-12 14:21:49 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by John Pappas on 10/12/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
