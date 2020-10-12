d5680a4ca2df0424fcd1905345bb57e116ec71ac
John Pappas <johncpappas4@gmail.com>
2020-10-12 14:51:34 -0400

    Completed Section 6.1 and 6.2

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by John Pappas on 10/12/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
