f2bb4b0d8fb6747d1ed8b3e5cb194796e2dc9f62
John Pappas <johncpappas4@gmail.com>
2020-10-12 15:36:43 -0400

    completed section 6.3 and 6.4 (with .gitignore0

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by John Pappas on 10/12/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyA-UDTgg1xs_cFFwYl66Iw4hN44M00KQNg"
+}
