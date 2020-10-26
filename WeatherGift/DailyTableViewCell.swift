f16a63e0fbc9e1e1bde7ee7932bbfa263a18fe58
John Pappas <johncpappas4@gmail.com>
2020-10-26 07:58:33 -0400

    Completed Section 6.15

---
+++ WeatherGift/DailyTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  DailyTableViewCell.swift
+//  WeatherGift
+//
+//  Created by John Pappas on 10/26/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+    @IBOutlet weak var dailyImageView: UIImageView!
+    @IBOutlet weak var dailyWeekdayLabel: UILabel!
+    @IBOutlet weak var dailyHighLabel: UILabel!
+    @IBOutlet weak var dailySummaryLabel: UITextView!
+    @IBOutlet weak var dailyLowLabel: UILabel!
+    
+    var dailyWeather: DailyWeather! {
+        didSet {
+            dailyImageView.image = UIImage(named: dailyWeather.dailyIcon)
+            dailyWeekdayLabel.text = dailyWeather.dailyWeekday
+            dailySummaryLabel.text = dailyWeather.dailySummary
+            dailyHighLabel.text = "\(dailyWeather.dailyHigh)°"
+            dailyLowLabel.text = "\(dailyWeather.dailyLow)°"
+            
+        }
+    }
+    
+}
